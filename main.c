d5682005558e693fc3f1db7718928e6e4f030ce4
Samuel Palmer <samiskramis@gmail.com>
2023-09-06 07:58:01 +0200

    Added: file to .gitignore

--- main.c
+++ main.c
@@ -4,11 +4,13 @@ int main()
 {
     SetTargetFPS(10);
     InitWindow(100, 100, "test");
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         DrawRectangle(25, 25, 50, 50, RED);
         EndDrawing();
     }
+
     return 0;
 }
