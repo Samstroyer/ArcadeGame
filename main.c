ff9519c950f3278fe36e7de1a6479522ae18c630
Samuel Palmer <samiskramis@gmail.com>
2023-09-06 18:02:02 +0200

    Added: Added alive variable and check when drawing

--- main.c
+++ main.c
@@ -8,16 +8,28 @@ typedef struct
     float speed;
 } Player;
 
+typedef struct
+{
+    float x;
+    float y;
+    float speed;
+    float damage;
+    bool alive;
+} Enemy;
+
 Texture2D player_texture;
 Texture2D enemy_texture;
 
 void LoadImages()
 {
     Image player_image = LoadImage("Sprites/player.png");
-    Image enemy_image = LoadImage("Sprites/player.png");
+    Image enemy_image = LoadImage("Sprites/enemy.png");
 
     ImageResize(&player_image, 25, 25);
     ImageResize(&enemy_image, 25, 25);
+
+    ImageRotateCCW(&enemy_image);
+    ImageRotateCCW(&enemy_image);
 
     player_texture = LoadTextureFromImage(player_image);
     enemy_texture = LoadTextureFromImage(enemy_image);
@@ -25,14 +37,6 @@ void LoadImages()
     UnloadImage(player_image);
     UnloadImage(enemy_image);
 }
-
-typedef struct
-{
-    float x;
-    float y;
-    float speed;
-    float damage;
-} Enemy;
 
 void Movement(Player *p)
 {
@@ -53,8 +57,23 @@ void RenderPlayer(Player *p)
     DrawTexture(player_texture, p->x, p->y, WHITE);
 }
 
-void RenderEnemies()
+void RenderEnemies(Enemy *enemies)
 {
+    for (int i = 0; i < 200; i++)
+    {
+        if (enemies[i].alive)
+        {
+            DrawTexture(enemy_texture, enemies[i].x, enemies[i].y, WHITE);
+        }
+    }
+}
+
+void SetupEnemy(Enemy *enemies)
+{
+    for (int i = 0; i < 200; i++)
+    {
+        enemies[i].alive = false;
+    }
 }
 
 int main()
@@ -62,6 +81,10 @@ int main()
     SetTargetFPS(60);
     InitWindow(800, 800, "Galaga 2.0");
     LoadImages();
+
+    Enemy enemies[200];
+    SetupEnemy(enemies);
+
     Player p;
     p.x = (GetScreenWidth() / 2) - (player_texture.width / 2);
     p.y = GetScreenHeight() - 40;
@@ -75,7 +98,7 @@ int main()
 
         Movement(&p);
         RenderPlayer(&p);
-        RenderEnemies();
+        RenderEnemies(enemies);
 
         EndDrawing();
     }
